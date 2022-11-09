5215b92d68d7a6424d09e04b18d5bdb88995c36f
ahmadrana24 <ahamdr84@gmail.com>
2022-11-09 19:53:43 +0500

    update

--- android/src/main/kotlin/com/djgeo/majascan/g_scanner/QrBorderView.kt
+++ android/src/main/kotlin/com/djgeo/majascan/g_scanner/QrBorderView.kt
@@ -44,18 +44,18 @@ class QrBorderView @JvmOverloads constructor(
         canvas?.drawLine(0f, height, width, height, mBorderPaint)
         canvas?.drawLine(width, 0f, width, height, mBorderPaint)
 
-        val cornerWidth = width / 5 //四角落外框寬度
+        val cornerWidth = 0 //四角落外框寬度
 
         //左上
-        canvas?.drawLine(0f, 0f, 0f, 0f, mPaint)
-        canvas?.drawLine(0f, 0f, 0f, 0f, mPaint)
+        canvas?.drawLine(0f, 0f, cornerWidth, 0f, mPaint)
+        canvas?.drawLine(0f, 0f, 0f, cornerWidth, mPaint)
 
         //右上
         canvas?.drawLine(width, 0f, width, 0f, mPaint)
-        canvas?.drawLine(width, 0f, width, 0f, mPaint)
+        canvas?.drawLine(width, 0f, width, cornerWidth, mPaint)
 
         //左下
-        canvas?.drawLine(0f, height, 0f, height, mPaint)
+        canvas?.drawLine(0f, height, cornerWidth, height, mPaint)
         canvas?.drawLine(0f, height, 0f, height, mPaint)
 
         //右下
